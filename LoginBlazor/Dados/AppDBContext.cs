7f7c10aba2088721a3d481e671c22a9a072cb55e
Usuario <Usuario@PC-GAMER-VINICI>
2024-09-18 20:45:19 -0300

    Adicionar arquivos de projeto.

---
+++ LoginBlazor/Dados/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿namespace LoginBlazor.Dados
+{
+    public class AppDBContext
+    {
+        private static List<User> _users = new List<User>();
+
+        public static bool SaveUser(User user)
+        {
+            bool isExist = _users.Any(x => x.Email == user.Email);
+            if (!isExist) {
+
+                _users.Add(user);
+                return true;
+            }
+            return false;
+        }
+
+        public class User
+        {
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+
+            public string Role { get; set; }
+
+        }
+    }
+}
---
+++ LoginBlazor/Program.cs
@@ -0,0 +1,45 @@
+using LoginBlazor.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)//Autenticador Utilizando o CookieAuthenticaton
+    .AddCookie(options =>
+    {
+
+        options.LoginPath = "/Login";
+        options.AccessDeniedPath = "/Acesso Negado";
+
+    });
+
+builder.Services.AddAuthorization();
+
+                                                                                         
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
